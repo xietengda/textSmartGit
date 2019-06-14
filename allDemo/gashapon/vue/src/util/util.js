af05605b4eccd0ab55225c9d08af6684e7b53b2e
Pangda <1002919580@qq.com>
2019-06-14 11:37:17 +0800

    添加工具类

--- allDemo/gashapon/vue/src/util/util.js
+++ allDemo/gashapon/vue/src/util/util.js
@@ -54,6 +54,28 @@ class util {
 //		console.log(manba().format("f"))
 		return manba(dateTime).format("l")
 	}
+
+
+	//将时间转成分xx钟xx小时xx秒
+	MillisecondToTime(msd) {
+		var time = parseFloat(msd) / 1000;   //先将毫秒转化成秒
+		if (null != time && "" != time) {
+			if (time > 60 && time < 60 * 60) {
+				time = parseInt(time / 60.0) + "分钟" + parseInt((parseFloat(time / 60.0) -
+					parseInt(time / 60.0)) * 60) + "秒";
+			}
+			else if (time >= 60 * 60 && time >= 60 * 60 * 1) {
+				time = parseInt(time / 3600.0) + "小时" + parseInt((parseFloat(time / 3600.0) -
+					parseInt(time / 3600.0)) * 60) + "分钟" +
+					parseInt((parseFloat((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60) -
+					parseInt((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60)) * 60) + "秒";
+			}
+			else {
+				time = parseInt(time) + "秒";
+			}
+		}
+		return time;
+	}
   	
   	//转换json格式
 	conJson(jsonString){
