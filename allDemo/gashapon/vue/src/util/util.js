a206265e476a0f396eb95353e2dfb116e176b2e5
Pangda <1002919580@qq.com>
2019-06-14 14:54:13 +0800

    更新工具类

--- allDemo/gashapon/vue/src/util/util.js
+++ allDemo/gashapon/vue/src/util/util.js
@@ -64,7 +64,14 @@ class util {
 				time = parseInt(time / 60.0) + "分钟" + parseInt((parseFloat(time / 60.0) -
 					parseInt(time / 60.0)) * 60) + "秒";
 			}
-			else if (time >= 60 * 60 && time >= 60 * 60 * 1) {
+			else if (time >= 60 * 60 && time >= 60 * 60 * 24) {
+				time = parseInt(parseInt(time / 3600.0)/24) + "天"+
+					parseInt(parseInt(time / 3600.0) - parseInt(parseInt(time / 3600.0)/24) * 24) + "小时" + 
+					parseInt((parseFloat(time / 3600.0) -parseInt(time / 3600.0)) * 60) + "分钟" +
+					parseInt((parseFloat((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60) -
+					parseInt((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60)) * 60) + "秒";
+			}
+			else if (time >= 60 * 60 && time >= 60 * 60 * 1 && time <= 60 * 60 * 24) {
 				time = parseInt(time / 3600.0) + "小时" + parseInt((parseFloat(time / 3600.0) -
 					parseInt(time / 3600.0)) * 60) + "分钟" +
 					parseInt((parseFloat((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60) -
